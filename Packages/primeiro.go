54c298eed6b8fbcc104165de11fa6ad0c345e554
Wiliender <wiliendersilva@gmail.com>
2022-04-04 16:31:15 -0300

    Structure package main, import package, func main

---
+++ Packages/primeiro.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+) // importação de pacotes
+
+func main() {
+	fmt.Println("Hello World")                                 //imprime na tela Hello World
+	fmt.Println(strings.Join([]string{"Hello", "World"}, " ")) //concatenação de strings com um espaço
+}
