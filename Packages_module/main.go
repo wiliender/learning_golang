5471043f39e8b46f41cfe9f402fe1d06c8626bdb
Wiliender <wiliendersilva@gmail.com>
2022-04-04 17:18:27 -0300

    instalando dependencia externa checkmail

--- Packages_module/main.go
+++ Packages_module/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo a partir do pacote main")
 	auxiliar.Escrever()
+
+	fmt.Println(checkmail.ValidateFormat("wiliendersilva"))
+	erro := (checkmail.ValidateFormat("wiliendersilva@gmail.com"))
+	fmt.Println(erro)
 }
