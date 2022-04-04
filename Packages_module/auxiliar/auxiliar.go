6e115308892ee62394784afeef58c10bf085bd07
Wiliender <wiliendersilva@gmail.com>
2022-04-04 17:04:39 -0300

    using the module, calling other packages and functions, public and private properties

---
+++ Packages_module/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+//This function is called by the main.go file where it logs a message to the screen
+func Escrever() {
+	fmt.Println("Escrevendo do pacote auxiliar")
+	escrever2()
+}
---
+++ Packages_module/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+func main() {
+	fmt.Println("Escrevendo a partir do pacote main")
+	auxiliar.Escrever()
+}
