2fa93985f4c2338d6d7abfcdefb0522e5b261ae4
Wiliender <wiliendersilva@gmail.com>
2022-04-06 00:30:15 -0300

    Advanced functions, named return

---
+++ Advanced_Functions/Named_Return/named_return .go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func calculosMatematicos(n1, n2 int) (soma int, subtracao int) {
+	soma = n1 + n2
+	subtracao = n1 - n2
+	return
+}
+
+func main() {
+	fmt.Println("Retorno Nomeado")
+
+	soma, subtracao := calculosMatematicos(10, 5)
+	fmt.Println(soma, subtracao)
+}
