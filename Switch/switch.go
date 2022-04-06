a50b78d8ea76b1527cda028fdfc81c8a24769fc7
Wiliender <wiliendersilva@gmail.com>
2022-04-06 00:02:00 -0300

    Switchs in golang

---
+++ Switch/switch.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda"
+	case 3:
+		return "Terça"
+	case 4:
+		return "Quarta"
+	case 5:
+		return "Quinta"
+	case 6:
+		return "Sexta"
+	case 7:
+		return "Sábado"
+	default:
+		return "Dia inválido"
+	}
+}
+
+func diaDaSemana2(numero int) string {
+	var diaDaSemana string
+
+	switch {
+	case numero == 1:
+		diaDaSemana = "Domingo"
+		//fallthrough for next case
+	case numero == 2:
+		diaDaSemana = "Segunda"
+	case numero == 3:
+		diaDaSemana = "Terça"
+	case numero == 4:
+		diaDaSemana = "Quarta"
+	case numero == 5:
+		diaDaSemana = "Quinta"
+	case numero == 6:
+		diaDaSemana = "Sexta"
+	case numero == 7:
+		diaDaSemana = "Sábado"
+	default:
+		diaDaSemana = "Dia inválido"
+	}
+	return diaDaSemana
+}
+
+func main() {
+	fmt.Println("Switch")
+	dia := diaDaSemana(100)
+	fmt.Println(dia)
+	fmt.Println("----------------------------------")
+	dia = diaDaSemana2(1)
+	fmt.Println(dia)
+}
